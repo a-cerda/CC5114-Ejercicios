e938884e9d23802717db2924eed8d01c590e543d
acerda <12897630+a-cerda@users.noreply.github.com>
2018-10-01 17:21:20 -0300

    Initial commit

--- Perceptron/Perceptron.py
+++ Perceptron/Perceptron.py
@@ -1,14 +1,16 @@
 
 
-
+#Perceptron class: implements a basic perceptron with the following fields:
+#weights: a python list of weights for the inputs
+#bias: the bias for the perceptron
+#inputs: the inputs for the calculation
 class Perceptron:
-    def __init__(self, bias, weight1, weight2):
-        self.weight1 = weight1
-        self.weight2 = weight2
+    def __init__(self, bias, weights):
+        self.weights = weights
         self.bias = bias
-        self.input1
-        self.input2
+        self.inputs
 
-    def think(self, input1, input2):
+
+    def think(self, inputs):
         pass
 
