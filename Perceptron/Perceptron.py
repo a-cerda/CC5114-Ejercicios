c9bf52dde34f1798c3a421be0f07aecc77d68429
acerda <12897630+a-cerda@users.noreply.github.com>
2018-10-01 17:14:23 -0300

    Initial commit

---
+++ Perceptron/Perceptron.py
@@ -0,0 +1,14 @@
+
+
+
+class Perceptron:
+    def __init__(self, bias, weight1, weight2):
+        self.weight1 = weight1
+        self.weight2 = weight2
+        self.bias = bias
+        self.input1
+        self.input2
+
+    def think(self, input1, input2):
+        pass
+
