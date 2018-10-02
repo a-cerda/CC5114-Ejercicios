c8de6dc9f18e4c8a188bb640dd242d0b2b8a9296
acerda <12897630+a-cerda@users.noreply.github.com>
2018-10-01 23:16:22 -0300

    Made perceptron class

--- Perceptron/Adder.py
+++ Perceptron/Adder.py
@@ -7,7 +7,16 @@ class Adder:
         self.perceptron2 = Perceptron(3,[2,2])
         self.perceptron3 = Perceptron(3,[2,2])
         self.perceptron4 = Perceptron(3,[2,2])
+        self.perceptroncarry = Perceptron(3,[2,2])
 
     def sum(self,input1,input2):
-        pass
+        result = []
+        output1 = self.perceptron1.feed([input1,input2])
+        output2 = self.perceptron2.feed([input1,output1])
+        output3 = self.perceptron3.feed([input2,output1])
+        sum = self.perceptron4.feed([output2,output3])
+        result.append(sum)
+        outputcarry = self.perceptroncarry.feed([output1,output1])
+        result.append(outputcarry)
+        return result
 
